53f36def83395915c0baf55db3eb1061926b3cd5
VDLPYL <63089508+VDLPYL@users.noreply.github.com>
2020-12-10 15:04:56 -0500

    Update myscript.js

--- tecnologia/computacion/lenguajes/JavaScript/js/myscript.js
+++ tecnologia/computacion/lenguajes/JavaScript/js/myscript.js
@@ -1,2 +1,2 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>"
+var time = new Date();
+console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
